e321a4874a58376c00dbe88c8354108b0d14cf40
flyrok <flyrok@gmail.com>
2019-04-30 13:43:46 -0400

    Updated version.py

--- fdsn_station/version.py
+++ fdsn_station/version.py
@@ -1,4 +1,4 @@
-progname='plan_b.py';
-__version_info__ = ('2019','04','16','01')
+progname='fdsn_station_info.py';
+__version_info__ = ('2019','04','29')
 __version__ = '.'.join(__version_info__)
 
