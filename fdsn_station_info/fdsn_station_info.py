c7b71d73475356ed97a22a33f7ad5421258b2a5e
flyrok <flyrok@gmail.com>
2019-05-27 12:19:57 -0400

    Merged Cameron's dist addition

--- fdsn_station_info/fdsn_station_info.py
+++ fdsn_station_info/fdsn_station_info.py
@@ -5,36 +5,37 @@ from obspy.clients.fdsn import Client
 import argparse
 from pathlib import Path
 import sys
+import math
 
 here = Path(__file__).resolve().parent
 exec(open(here / "version.py").read())
 
-def dump_output(inv,output,debug):
+def dump_output(inv,output,debug,lon0,lat0):
     '''
     Dumps station meta data to a text file and an ObsPy Inventory
     The text file does not include the response info that is
     included in the Inventory
 
-    :type  inv: Obspy Inventory 
+    :type  inv: Obspy Inventory
     :param inv:  This is the inventory returned from
-        FDSN server. 
+        FDSN server.
 
     :type output: str, filename
-    :param output: This is the output filename for the human 
+    :param output: This is the output filename for the human
         readable text file. For the StationXml file, the
         suffix is replaced with staxml
 
     :type debug: int, required
     :param debug: specifies debug verbosity
     '''
-   
+
     # Save the inventory as a StationXML file
     xmlout=f"{Path(output).stem}.staxml"
     if debug: print("Saving staxml: ",xmlout)
     inv.write(xmlout,format="STATIONXML")
 
     msgs=[]
-    msg=f" N,   sta, loc,   chan,       lat,        lon,   elev,  depth,    sampr,  hang, vang,  sensor\n"
+    msg=f" N,   sta, loc,   chan,       lat,        lon,   elev, dist (km),  depth,    sampr,  hang, vang,  sensor\n"
     msgs.append(msg)
 
     if debug: print(msg)
@@ -43,6 +44,7 @@ def dump_output(inv,output,debug):
     # at the channel level
     for i in range(0,len(inv.networks)): # iter networks
         net=inv.networks[i]
+
         net_code=net._code
         for j in range(0,len(net)): # iter stations
             sta=net[j]
@@ -59,7 +61,8 @@ def dump_output(inv,output,debug):
                 az=chan._azimuth
                 dip=chan._dip
                 sensor=chan.sensor.description
-                msg=f"{net_code:2s}, {sta_code:>6s}, {loc:>2s}, {code:>6s}, {lat:>9.6f}, {lon:>10.6f}, {elev:>6.1f}, {dep:>6.1f}, {sampr:>8.4f}, {az:>5.1f},{dip:>4.1f}, {sensor}\n"
+                dist= latlongdist(lat0,lat,lon0,lon)
+                msg=f"{net_code:2s}, {sta_code:>6s}, {loc:>2s}, {code:>6s}, {lat:>9.6f}, {lon:>10.6f}, {elev:>6.1f}, {dist}, {dep:>6.1f}, {sampr:>8.4f}, {az:>5.1f},{dip:>4.1f}, {sensor}\n"
 
                 if debug: print(msg)
 
@@ -67,7 +70,26 @@ def dump_output(inv,output,debug):
     file = open(output,"w+")
     for i in msgs:
         file.write(i)
-    file.close() 
+    file.close()
+
+def latlongdist(phi1,phi2,gamma1,gamma2):
+    # Calculates distance between 2 points on a sphere
+    # Note: Earth is not a perfect sphere, so calculations will have some error
+
+    R = 6371; # Average radius of the earth (km)
+
+    phi1 = math.radians(phi1)
+    phi2 = math.radians(phi2)
+    gamma1 = math.radians(gamma1)
+    gamma2 = math.radians(gamma2)
+
+    phi_dif = phi1-phi2
+    gamma_dif = gamma1-gamma2
+
+    a =  math.sin(phi_dif/2)**2+math.cos(phi1)*math.cos(phi2)*math.sin(gamma_dif/2)**2
+    c = 2*math.atan2(math.sqrt(a),math.sqrt(1-a))
+    d = R*c;
+    return d
 
 def main():
     '''
@@ -79,10 +101,10 @@ def main():
             Grab station metadata from IRIS FDSN server for stations that are: 1) within a
             certain search radius of a given lat,lon, and 2) operating during a
             particular time frame. It outputs two files: a CSV file
-            and a StationXML file. 
+            and a StationXML file.
             ''',
             epilog='''
-            
+
             ''')
 
     parser.add_argument("-b","--begin", type=str,
@@ -96,25 +118,25 @@ def main():
         required=False, help="Net code. Used to narrow search results. Used to narrow search results ")
 
     parser.add_argument("-c","--chan", type=str,default=None,
-        required=False, help="""Chan codes, defaults to all available. E.g. 
+        required=False, help="""Chan codes, defaults to all available. E.g.
             ``BH?,HH?,*H*``. Used to narrow search results """)
 
     parser.add_argument("-r","--resp", action="store_true",default=False,
         required=False, help="Set to include response in StationXML file.")
 
-    parser.add_argument("--lon", type=float,required=True, 
+    parser.add_argument("--lon", type=float,required=True,
         help="Center longitude for search radius. Decimal degrees.")
 
-    parser.add_argument("--lat", type=float,required=True, 
+    parser.add_argument("--lat", type=float,required=True,
         help="Center latitude for search radius. Decimal degrees.")
 
-    parser.add_argument("--radmin", type=float,required=True, 
+    parser.add_argument("--radmin", type=float,required=True,
         help="Minimum search radius in km (>0)")
 
-    parser.add_argument("--radmax", type=float,required=True, 
+    parser.add_argument("--radmax", type=float,required=True,
         help="Maximum search radius in km (>radmin)")
 
-    parser.add_argument("-o","--output", type=str,required=False, 
+    parser.add_argument("-o","--output", type=str,required=False,
         default="sta_info.csv", help="Output filename for CSV. CSV suffix is replaced with .staxml for StationXML format")
 
     parser.add_argument("-v", "--verbose", action="count",default=0,
@@ -137,10 +159,11 @@ def main():
     output=args.output
     debug=args.verbose
 
+
     if do_resp:
         level='response'
     else:
-         level='channel'     
+         level='channel'
 
     if debug > 0:
         print('Command line arguments....')
@@ -153,15 +176,14 @@ def main():
         print("radmin: ",radmin)
         print("radmax: ",radmax)
         print("level: ",level)
-    
+
     client = Client(timeout=240,base_url="http://service.iris.edu")
     try:
         inv=client.get_stations(starttime=startt,endtime=endt,network=net,channel=chan,
             latitude=lat,longitude=lon,minradius=radmin,maxradius=radmax,level=level)
-        dump_output(inv,output,debug)
+        dump_output(inv,output,debug, lon, lat)
     except Exception as e:
         print(e)
 
 if __name__ == '__main__':
     main()
-
